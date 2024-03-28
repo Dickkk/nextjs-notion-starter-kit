0dfd7c8c8529208ed013c9c7be84347986e923af
chaoice <12228095+chaoice@users.noreply.github.com>
2024-03-28 15:52:47 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -4,22 +4,26 @@ export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '7875426197cf461698809def95960ebf',
 
+  rootNotionPageId: '44692b67647a4c4ba3904081ecf488db',
+
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Allen 的博客',
+  domain: 'https://chaoice3240.notion.site/44692b67647a4c4ba3904081ecf488db',
+  author: 'Allen Zhang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Example site description',
+  socialImageTitle: 'Tuomas',
+  socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'chaoice3241',
+  github: 'chaoice',
+  linkedin: 'chaoice3240',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
